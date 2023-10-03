4de9e463fffe624bbf4a2e98508475a22a448855
Rafael Spíndola <rafa.spin95@gmail.com>
2023-10-03 12:06:33 -0300

    Project created

---
+++ src/main/java/com/attornatus/addressapi/AddressapiApplication.java
@@ -0,0 +1,13 @@
+package com.attornatus.addressapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/attornatus/addressapi/AddressapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attornatus.addressapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
