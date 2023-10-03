48bfe7196a495ff862696dba4ead026aaa0b4ad8
Rafael Spíndola <rafa.spin95@gmail.com>
2023-10-03 12:14:58 -0300

    Created a PessoaService

---
+++ src/main/java/com/attornatus/addressapi/service/PessoaService.java
@@ -0,0 +1,17 @@
+package com.attornatus.addressapi.service;
+
+import com.attornatus.addressapi.models.Pessoa;
+import com.attornatus.addressapi.repository.PessoaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PessoaService {
+
+    @Autowired
+    private PessoaRepository pessoaRepository;
+
+    public Pessoa criarPessoa(Pessoa pessoa) {
+        return pessoaRepository.save(pessoa);
+    }
+}
